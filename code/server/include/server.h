05b3117732d37e254102780a280e88e2e4eeecf0
jwimd <chenjiewei@zju.edu.cn>
2022-10-03 22:17:43 +0800

    init project

---
+++ code/server/include/server.h
@@ -0,0 +1,22 @@
+/*
+ * @Author: jwimd
+ * @Date: 2022-10-03 21:19:15
+ * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
+ * @LastEditTime: 2022-10-03 21:38:37
+ * @FilePath: /code/server/include/server.h
+ * @Description: socket服务端的头文件
+ */
+
+#pragma once
+
+#include <iostream>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+class Server
+{
+public:
+    Server() = delete;
+
+protected:
+};
---
+++ code/server/server.cpp
@@ -0,0 +1,10 @@
+/*
+ * @Author: jwimd
+ * @Date: 2022-10-03 20:33:10
+ * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
+ * @LastEditTime: 2022-10-03 21:49:47
+ * @FilePath: /code/server/server.cpp
+ * @Description: server类函数定义
+ */
+
+#include <server.h>
