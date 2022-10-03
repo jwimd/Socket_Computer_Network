cc3e0658b5aeb15ce029215fb11d81ff24582c2f
jwimd <chenjiewei@zju.edu.cn>
2022-10-03 22:22:58 +0800

    init project

---
+++ src/client/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Author: jwimd chenjiewei@zju.edu.cn
+ * @Date: 2022-10-03 20:34:07
+ * @LastEditors: jwimd chenjiewei@zju.edu.cn
+ * @LastEditTime: 2022-10-03 22:21:39
+ * @FilePath: /Socket_Computer_Network/src/client/main.cpp
+ * @Description: 客户端主函数
+ */
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "client!" << std::endl;
+    return 0;
+}
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,8 +1,8 @@
 /*
- * @Author: error: git config user.name && git config user.email & please set dead value or install git
+ * @Author: jwimd
  * @Date: 2022-10-03 21:30:48
- * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
- * @LastEditTime: 2022-10-03 21:51:33
+ * @LastEditors: jwimd chenjiewei@zju.edu.cn
+ * @LastEditTime: 2022-10-03 22:21:08
  * @FilePath: /code/server/main.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
