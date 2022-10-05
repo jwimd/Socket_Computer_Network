6f58c5d56e84f58938f9ebbadde236e0b0a56f39
jwimd <chenjiewei@zju.edu.cn>
2022-10-05 09:50:39 +0800

    add class thread

--- src/thread/include/thread.h
+++ src/thread/include/thread.h
@@ -1,11 +1,16 @@
 /***
- * @Description:
+ * @Description: 线程相关定义
  * @Author: jwimd chenjiewei@zju.edu.cn
  * @Date: 2022-10-04 22:14:11
  * @LastEditors: jwimd chenjiewei@zju.edu.cn
  * @LastEditTime: 2022-10-04 22:20:05
  * @FilePath: /Socket_Computer_Network/src/thread/include/thread.h
  */
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <functional>
+
 #include "type.h"
 
 class Thread
